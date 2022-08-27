e759822bcb925fbf14e465d472466b50fcfe7751
S2mple1 <111256885+S2mple1@users.noreply.github.com>
2022-08-27 18:04:15 +0800

    Update op.js

--- 主体/sites/s1/op.js
+++ 主体/sites/s1/op.js
@@ -22,6 +22,15 @@ var op =
 };
 function MusicPlayer() {
 	var music=document.getElementById("music");
-	var status=1;
 	music.play();
+    music.loop=false;
+    music.preload=true;
+    music.volume=1;
+}
+function MusicPlayer2() {
+	var music=document.getElementById("music2");
+	music.play();
+    music.loop=false;
+    music.preload=true;
+    music.volume=1;
 }
