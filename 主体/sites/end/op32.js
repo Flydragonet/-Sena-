973cbe776c9e25cc13a09ffeff1fe091a07fea3c
Flydragonet <flit.dragon233@gmail.com>
2022-08-28 10:17:44 +0800

    结局bug修复

--- 主体/sites/end/op32.js
+++ 主体/sites/end/op32.js
@@ -1,11 +1,11 @@
 var op = 
 {   "type":0,
 
-    "nextpage":"index.html",
+    "nextpage":"../../登录/menu.html",
 
-    "steps":10,
+    "steps":11,
     
-    "name":["Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","我","我","Sena酱",],
+    "name":["Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","我","我","Sena酱","旁白"],
     
     "texts":["唉，事到如今，我也不能嘴硬了呢","你这人啊",
             "这种事直接跟我说就好了嘛",
@@ -13,12 +13,13 @@ var op =
             "像今天这种事，如果你以前不担心这担心那，直接跟我说，不就不会发生了吗",
             "以后，跟我在一块，放开一点",
             "你看我是不是有什么事都跟你说","好…………我明白了",
-        "那么，你的意思是答应我的告白了？","呵呵，暂时答应了吧，之后还是看你表现呢"],
+        "那么，你的意思是答应我的告白了？","呵呵，暂时答应了吧，之后还是看你表现呢",
+            "恭喜！你达成了好结局"],
     
-    "small":["none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",],
+    "small":["none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none"],
 
-    "big":["none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",],
+    "big":["none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none"],
 
-    "bigpos":["none","none","none","none","none","none","none","none","none","none","none","none","none","none","none",]
+    "bigpos":["none","none","none","none","none","none","none","none","none","none","none","none","none","none","none","none"]
 };
 
--- 主体/sites/end/op34.js
+++ 主体/sites/end/op34.js
@@ -1,11 +1,11 @@
 var op = 
 {   "type":0,
 
-    "nextpage":"index.html",
+    "nextpage":"../../登录/menu.html",
 
-    "steps":9,
+    "steps":10,
     
-    "name":["Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","旁白"],
+    "name":["Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","Sena酱","旁白","旁白"],
     
     "texts":["呵呵……",
             "你这个人，有什么事，总是藏着掖着",
@@ -15,12 +15,13 @@ var op =
             "总之……",
             "你还是放下这段感情吧",
             "这样对我们俩都好",
-            "Sena酱摆了摆手，离开了",],
+            "Sena酱摆了摆手，离开了",
+            "你达成了坏结局，仍需努力！"],
     
-    "small":["none","none","none","none","none","none","none","none","none","none","none","none","none",],
+    "small":["none","none","none","none","none","none","none","none","none","none","none","none","none","none"],
 
-    "big":["none","none","none","none","none","none","none","none","none","none","none","none","none",],
+    "big":["none","none","none","none","none","none","none","none","none","none","none","none","none","none"],
 
-    "bigpos":["none","none","none","none","none","none","none","none","none","none","none","none","none",]
+    "bigpos":["none","none","none","none","none","none","none","none","none","none","none","none","none","none"]
 };
 
